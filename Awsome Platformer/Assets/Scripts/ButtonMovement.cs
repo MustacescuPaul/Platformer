096d3985dae85b53a95157acee83b1ac0eb4d94d
MustacescuPaul <mustacescupaul@yahoo.ro>
2016-01-19 23:19:02 +0200

    script v1

---
+++ Awsome Platformer/Assets/Scripts/ButtonMovement.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonMovement : TouchManager
+{
+    public enum type {LeftButton,RightButton,JumpButton};
+    public type buttonType;
+
+    public GameObject playerObject = null;
+	
+}
---
+++ Awsome Platformer/Assets/Scripts/Touch Manager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchManager : MonoBehaviour
+{
+    public static bool guiTouch = false;
+
+    public void TouchInput(GUITexture texture)
+    {
+        if (texture.HitTest(Input.GetTouch(0).position))
+        {
+            switch (Input.GetTouch(0).phase)
+            {
+                case TouchPhase.Began:
+                    SendMessage("OnFirstTouchBegan");
+                    guiTouch = true;
+                    break;
+                case TouchPhase.Moved:
+                    SendMessage("OnFirstTouchMoved");
+                    guiTouch = true;
+                    break;
+                case TouchPhase.Stationary:
+                    SendMessage("OnFirstTouchStayed");
+                    guiTouch = true;
+                    break;
+                case TouchPhase.Ended:
+                    SendMessage("OnFirstTouchEnded");
+                    guiTouch = true;
+                    break;
+            }
+
+        }
+        if (texture.HitTest(Input.GetTouch(0).position))
+        {
+            switch (Input.GetTouch(1).phase)
+            {
+                case TouchPhase.Began:
+                    SendMessage("OnSecondTouchBegan");
+                    break;
+                case TouchPhase.Moved:
+                    SendMessage("OnSecondTouchMoved");
+                    break;
+                case TouchPhase.Stationary:
+                    SendMessage("OnSecondTouchStayed");
+                    break;
+                case TouchPhase.Ended:
+                    SendMessage("OnSecondTouchEnded");
+                    break;
+            }
+
+        }
+    }
+}
+
+
